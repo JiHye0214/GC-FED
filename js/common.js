82efc17f1b57090af17294619f16cd3776f70956
JiHye0214 <qkrwlgp1526@gmail.com>
2025-03-01 12:21:08 -0800

    250301 : myportfolio about complete

--- js/common.js
+++ js/common.js
@@ -1,14 +1,21 @@
 let url = window.location.pathname;
 let currentPage = url.substring(1).split(".")[0];
-console.log(currentPage)
 
 // NAV 
 const $navWrap = document.querySelectorAll("nav > a");
+
 $navWrap.forEach((nav) => {
-    const navText = nav.innerText == "Home" ? "index" : nav.innerText.toLowerCase();
+    const navText = 
+    nav.innerText === "Home" ? "index" : 
+    nav.innerText === "Contact Me" ? "contact" : 
+    nav.innerText.toLowerCase();
 
     if(navText == currentPage) {
-        $(nav).css("borderBottom", "3px solid var(--my-yellow)");
+        if(navText == "contact") {
+            $(nav).css("backgroundColor", "var(--yellow-hover)");
+        } else {
+            $(nav).css("borderBottom", "3px solid var(--my-yellow)");
+        }
     } else {
         $(nav).css("color", "");
     }
